db2fbba69d34d67c0c128d098ae3c205883920bf
zangziqin <63489644+DuanYang5@users.noreply.github.com>
2026-05-15 14:49:16 +0800

    update

--- quark_signin.py
+++ quark_signin.py
@@ -1,8 +1,9 @@
 """
-cron "8 12 * * *" script-path=xxx.py,tag=匹配cron用
+script-path=xxx.py,tag=匹配cron用
 抓取地址https://pan.quark.cn/
 环境变量：
     QUARK_COOKIE      多账号用 &&或回车分隔
+cron 8 12 * * *
 new Env('夸克签到')
 """
 import os
